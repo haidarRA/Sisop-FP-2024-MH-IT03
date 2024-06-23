724db389ea31a916050d57dbfad983dabc0c78a8
Haidar Rafi A <149871906+haidarRA@users.noreply.github.com>
2024-06-23 23:34:27 +0700

    Create monitor.c

---
+++ fp/monitor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+#include <time.h>
+#include <stdbool.h>
+#include <libgen.h>
+
+int main() {
+  //mohon maaf kami masih belum membuat program untuk monitor chat pada room karena keterbatasan waktu yang kami alami.
+  return 0;
+}
